46a3b4b2c8a3ae20b2971af1e68233bfcc5394d2
Robson da Silva <sc.robson@gmail.com>
2016-10-26 18:04:40 -0200

    Fixing js structure

--- MyMoods/wwwroot/app/js/app.js
+++ MyMoods/wwwroot/app/js/app.js
@@ -1,82 +1,102 @@
-﻿(function () {
-    'use strict';
+﻿$(document).ready(function () {
+    activate();
+});
 
-    var model = null;
+var model = null;
+var postModel = null;
 
-    $.get('/api/forms/57976abd266b3c042d6217f6/metadata')
-        .then(function (response) {
-            load(response);
-        }, function (response) {
-            alert(response);
-        });
+function activate() {
 
-    function load(data) {
-        model = data;
+    postModel = {
+        selectedMood: null
+    };
 
-        var container = $('#main-container');
-        injectMoodsPanel(container, model.moods);
-        injectTagsPanel(container, model.tags);
-        injectQuestionsPanel(container, model.questions);
-    }
+    getForm('57976abd266b3c042d6217f6');
+}
 
-    function injectMoodsPanel(container, moods) {
+function getForm(formId) {
 
-        var html = '';
+    var promise = $.get('/api/forms/' + formId + '/metadata');
 
-        html += '<div class="panel moods-panel">';
-        html += '<div class="panel-header moods-panel-header">';
-        html += 'Como você está se sentindo?';
+    promise.then(function (response) {
+        loadForm(response);
+    }, function (response) {
+        alert(response);
+    });
+}
+
+function loadForm(data) {
+
+    model = data;
+
+    var container = $('#main-container');
+    injectMoodsPanel(container, model.moods);
+    injectTagsPanel(container, model.tags);
+    injectQuestionsPanel(container, model.questions);
+}
+
+function injectMoodsPanel(container, moods) {
+
+    var html = '';
+
+    html += '<div class="panel moods-panel">';
+    html += '<div class="panel-header moods-panel-header">';
+    html += 'Como você está se sentindo?';
+    html += '</div>';
+
+    moods.forEach(function (mood) {
+        html += '<img id="testando" src="' + mood.image + '" class="mood-image" onclick="selectMood(\'' + mood.value + '\')"></img>';
+    }, this);
+
+    html += '</div>';
+
+    container.append(html);
+
+    $("#testando").click(selectMood(1));
+}
+
+function injectTagsPanel(container, tags) {
+
+    var html = '';
+
+    html += '<div class="panel tags-panel">';
+    html += '<div class="panel-header tags-panel-header">';
+    html += 'Tags';
+    html += '</div>';
+
+    tags.forEach(function (tag) {
+        html += '<div class="tag">' + tag.title + '</div>';
+    }, this);
+
+    html += '</div>';
+
+    container.append(html);
+}
+
+function injectQuestionsPanel(container, questions) {
+
+    var html = '';
+
+    questions.forEach(function (question) {
+
+        html += '<div class="panel questions-panel">';
+        html += '<div class="panel-header questions-panel-header">';
+        html += '<span>' + question.title + '</span>';
         html += '</div>';
 
-        moods.forEach(function (mood) {
-            html += '<img src="' + mood.image + '" class="mood-image"></img>';
-        }, this);
-
-        html += '</div>';
-
-        container.append(html);
-    }
-
-    function injectTagsPanel(container, tags) {
-
-        var html = '';
-
-        html += '<div class="panel tags-panel">';
-        html += '<div class="panel-header tags-panel-header">';
-        html += 'Tags';
-        html += '</div>';
-
-        tags.forEach(function (tag) {
-            html += '<div class="tag">' + tag.title + '</div>';
-        }, this);
-
-        html += '</div>';
-
-        container.append(html);
-    }
-
-    function injectQuestionsPanel(container, questions) {
-
-        var html = '';
-
-        questions.forEach(function (question) {
-
-            html += '<div class="panel questions-panel">';
-            html += '<div class="panel-header questions-panel-header">';
-            html += '<span>' + question.title + '</span>';
+        if (question.type == 'text') {
+            html += '<div class="padded-holder">';
+            html += '<textarea class="free-text"></textarea>';
             html += '</div>';
+        }
 
-            if (question.type == 'text') {
-                html += '<div class="padded-holder">';
-                html += '<textarea class="free-text"></textarea>';
-                html += '</div>';
-            }
+        html += '</div>';
 
-            html += '</div>';
+    }, this);
 
-        }, this);
+    container.append(html);
+}
 
-        container.append(html);
-    }
-
-})();
+function selectMood(mood) {
+    postModel.selectedMood = mood;
+}
