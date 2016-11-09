5ee534438a6747c9f23fc22ef6bbd70feb8a9663
Robson da Silva <sc.robson@gmail.com>
2016-11-09 00:37:59 -0200

    Refactoring DTO

--- MyMoods/Domain/DTO/FormMetadataDTO.cs
+++ MyMoods/Domain/DTO/FormMetadataDTO.cs
@@ -7,13 +7,13 @@ namespace MyMoods.Domain.DTO
     {
         public FormMetadataDTO(Form form, Company company, IList<Question> questions, IList<Tagg> tags, IList<MoodDTO> moods)
         {
-            Form = new FormDTO(form, company);
+            Form = new FormWithCompanyDTO(form, company);
             Questions = questions.Select(x => new QuestionDTO(x)).ToList();
             Tags = tags.Select(x => new TagDTO(x)).ToList();
             Moods = moods;
         }
 
-        public FormDTO Form { get; private set; }
+        public FormWithCompanyDTO Form { get; private set; }
         public IList<QuestionDTO> Questions { get; private set; }
         public IList<TagDTO> Tags { get; private set; }
         public IList<MoodDTO> Moods { get; private set; }
