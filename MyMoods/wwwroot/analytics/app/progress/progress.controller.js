c31b513900058d3440397357dfa65c5f62f44bb8
Robson <sc.robson@gmail.com>
2017-02-19 20:19:19 -0300

    Adding auth ping in the progress controller

--- MyMoods/wwwroot/analytics/app/progress/progress.controller.js
+++ MyMoods/wwwroot/analytics/app/progress/progress.controller.js
@@ -4,11 +4,18 @@
 
     progress.controller('ProgressController', progressController);
 
-    progressController.$inject = [];
+    progressController.$inject = ['$http', 'APP_CONFIG'];
 
     /* @ngInject */
-    function progressController() {
+    function progressController($http, APP_CONFIG) {
 
         var vm = this;
+
+        $http.get(APP_CONFIG.API_BASE_URL + '/ping')
+            .then(function(){
+                //Just for the 401
+            }, function(){
+                //Just for the 401
+            });
     }
 })();
