43682506116c0772f02ab48ab2a814d1c89dd691
Robson <sc.robson@gmail.com>
2017-03-05 15:26:48 -0300

    Changing mailer service to use hangfire

--- MyMoods/Contracts/IMailerService.cs
+++ MyMoods/Contracts/IMailerService.cs
@@ -4,6 +4,7 @@ namespace MyMoods.Contracts
 {
     public interface IMailerService
     {
+        void Send(string to, string subject, string body);
         void SendResetedPassword(User user, string password);
     }
 }
--- MyMoods/Services/MailerService.cs
+++ MyMoods/Services/MailerService.cs
@@ -1,5 +1,7 @@
-﻿using MailKit.Net.Smtp;
+﻿using Hangfire;
+using MailKit.Net.Smtp;
 using MailKit.Security;
+using Microsoft.Extensions.Configuration;
 using MimeKit;
 using MyMoods.Contracts;
 using MyMoods.Domain;
@@ -10,25 +12,39 @@ namespace MyMoods.Services
 {
     public class MailerService : IMailerService
     {
-        private void Send(MimeMessage message)
+        private readonly IConfigurationRoot _settings;
+
+        public MailerService(IConfigurationRoot settings)
         {
-            using (var client = new SmtpClient())
-            {
-                client.LocalDomain = "mymoods.co";
-                client.Connect("mail.privateemail.com", 465, SecureSocketOptions.SslOnConnect);
-                client.Authenticate("contato@mymoods.co", "Teste123.");
-                client.Send(message);
-                client.Disconnect(true);
-            }
+            _settings = settings;
         }
 
         private MimeMessage CreateMessage(string to, string subject, string body)
         {
             var mailTo = new List<MailboxAddress>() { new MailboxAddress(to) };
-            var mailFrom = new List<MailboxAddress>() { new MailboxAddress("contato@mymoods.co") };
+            var mailFrom = new List<MailboxAddress>() { new MailboxAddress("My Moods", "contato@mymoods.co") };
             var mailBody = new TextPart("html") { Text = body };
 
             return new MimeMessage(mailFrom, mailTo, subject, mailBody);
+        }
+
+        public void Send(string to, string subject, string body)
+        {
+            var message = CreateMessage(to, subject, body);
+
+            using (var client = new SmtpClient())
+            {
+                var section = _settings.GetSection("Email");
+                var host = section.GetValue<string>("Host");
+                var port = section.GetValue<int>("Port");
+                var user = section.GetValue<string>("Username");
+                var pass = section.GetValue<string>("Password");
+
+                client.Connect(host, port, SecureSocketOptions.SslOnConnect);
+                client.Authenticate(user, pass);
+                client.Send(message);
+                client.Disconnect(true);
+            }
         }
 
         public void SendResetedPassword(User user, string password)
@@ -48,9 +64,7 @@ namespace MyMoods.Services
             builder.Append($"<br>");
             builder.Append($"<b>My Moods</b>");
 
-            var message = CreateMessage(user.Email, "Nova senha", builder.ToString());
-
-            Send(message);
+            BackgroundJob.Enqueue<IMailerService>(x => x.Send(user.Email, "Nova senha", builder.ToString()));
         }
     }
 }
